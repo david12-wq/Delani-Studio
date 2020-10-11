d25ba938109d59926eca0ba530fb87118afb31e8
david12-wq <davidkamau245@gmail.com>
2020-10-11 23:08:48 +0300

    modification chenges index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-$(document).ready(function () {
-    $("#davvy").click(function () {
-        $("#davvy").hide();
-        $(".done1").show();
-    });
-    $(".done1").click(function () {
-        $(".done1").hide();
-        $("#davvy").show();
-    });
-});
+
+function myFunction() {
+    var name = document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+
+    if ((name)&&(email)){
+        alert(name +"thank you");
+    }
+    else{
+        alert("fill the form")
+    }
+    
+}
+    
+
+    
+
+
+
